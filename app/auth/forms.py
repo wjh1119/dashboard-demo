23aaae02c2dd986f38a84b896ef57997e5656e4c
wjh2016 <524022279@qq.com>
2019-03-07 10:17:18 +0800

    feature: Cancel the boolean field 'keep me logged in'

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -9,7 +9,6 @@ class LoginForm(FlaskForm):
     email = StringField('电子邮箱', validators=[DataRequired(), Length(1, 64),
                                              Email()])
     password = PasswordField('密码', validators=[DataRequired()])
-    remember_me = BooleanField('保持登录')
     submit = SubmitField('登录')
 
 
