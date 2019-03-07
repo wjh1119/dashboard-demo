77084616713d0408613865b772d248f2b9b07dd5
wjh2016 <524022279@qq.com>
2019-03-07 15:59:15 +0800

    fix: fix the problem that can't login

--- app/auth/views.py
+++ app/auth/views.py
@@ -13,7 +13,7 @@ def login():
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
         if user is not None and user.verify_password(form.password.data):
-            login_user(user, form.remember_me.data)
+            login_user(user)
             next = request.args.get('next')
             if next is None or not next.startswith('/'):
                 next = url_for('main.minimal_dashboard')
